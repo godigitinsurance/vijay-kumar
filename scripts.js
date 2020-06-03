3e7307e86dbc7f7a31958f2fc860355247f91b17
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-06-03 11:50:10 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Vijay Kumar");
+$("#policyNumber").text("IM05409205");
+$("#startDate").text("04-Jun-2020");
+$("#endDate").text("03-Jun-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DX2445");
+$("#make").text("BAJAJ");
+$("#model").text("V15");
+$("#vehicalSubType").text("STD (150 CC)");
+$("#engineNo").text("JHYRHF21247");
+$("#chassis").text("MD2A74BY3HRF04265");
